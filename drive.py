dd4a5276ff8316779b4f560f6bbdd30e46393fde
AB <alexandre.barilov@nauto.com>
2017-01-28 16:09:56 -0800

    change to 60x60

--- drive.py
+++ drive.py
@@ -1,6 +1,7 @@
 import argparse
 import base64
 import json
+import cv2
 
 import numpy as np
 import socketio
@@ -14,16 +15,26 @@ from io import BytesIO
 
 from keras.models import model_from_json
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array
-
-# Fix error with Keras and TensorFlow
 import tensorflow as tf
 tf.python.control_flow_ops = tf
+
+def preprocess(image):
+    # get shape and chop off 1/3 from the top
+    shape = image.shape
+    # note: numpy arrays are (row, col)!
+    image = image[shape[0]/4:shape[0]-25, 0:shape[1]]
+    #image = cv2.resize(image, (200,66), interpolation=cv2.INTER_AREA)
+    image = cv2.resize(image, (64,64), interpolation=cv2.INTER_AREA)
+    return image
+
 
 
 sio = socketio.Server()
 app = Flask(__name__)
 model = None
 prev_image_array = None
+
+
 
 @sio.on('telemetry')
 def telemetry(sid, data):
@@ -36,12 +47,13 @@ def telemetry(sid, data):
     # The current image from the center camera of the car
     imgString = data["image"]
     image = Image.open(BytesIO(base64.b64decode(imgString)))
-    image_array = np.asarray(image)
+    image_pre = np.asarray(image)
+    image_array = preprocess(image_pre)
     transformed_image_array = image_array[None, :, :, :]
     # This model currently assumes that the features of the model are just the images. Feel free to change this.
-    steering_angle = float(model.predict(transformed_image_array, batch_size=1))
+    steering_angle = 1.0*float(model.predict(transformed_image_array, batch_size=1))
     # The driving model currently just outputs a constant throttle. Feel free to edit this.
-    throttle = 0.2
+    throttle = 0.3
     print(steering_angle, throttle)
     send_control(steering_angle, throttle)
 
@@ -65,14 +77,7 @@ if __name__ == '__main__':
     help='Path to model definition json. Model weights should be on the same path.')
     args = parser.parse_args()
     with open(args.model, 'r') as jfile:
-        # NOTE: if you saved the file by calling json.dump(model.to_json(), ...)
-        # then you will have to call:
-        #
-        #   model = model_from_json(json.loads(jfile.read()))\
-        #
-        # instead.
-        model = model_from_json(jfile.read())
-
+        model = model_from_json(json.load(jfile))
 
     model.compile("adam", "mse")
     weights_file = args.model.replace('json', 'h5')
